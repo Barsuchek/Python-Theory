738f2158f5d9ebf1c064d58b16eeab2f230161d6
Dmitriy Ka <rakbarcik@yandex.ru>
2023-02-25 20:11:43 +0300

    Update odd.py
    Исправление программы для тренировки

--- ProjectsForTraining/Topic1/odd.py
+++ ProjectsForTraining/Topic1/odd.py
@@ -7,6 +7,6 @@ odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
 right_this_minute = datetime.today().minute
 
 if right_this_minute in odds:
-    print("Эта минута кажется немного странной.")
+    print("Нечетное число минут.")
 else:
-    print("Ни одной лишней минуты.")
+    print("Четное число минут.")
